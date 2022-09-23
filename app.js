414aa13d7870ded78310df6515c908893be6362a
thamiko1 <thamiko123@gmail.com>
2022-09-23 22:48:59 +0800

    add bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
 
 mongoose.connect('mongodb://localhost:27017/online-learning',{
     useNewUrlParser: true,
@@ -14,11 +16,12 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const app = express();
+
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res)=> {
     res.render('home')
 })
