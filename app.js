afe294416d6a65b9e370a461d0cfb517166afe4f
Aquos06 <ruliciohansen@gmail.com>
2022-09-25 18:53:15 +0800

    picture is not showing and have white space on the left

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 
-mongoose.connect('mongodb://localhost:27017/online-learning',{
-    useNewUrlParser: true,
-    // useCreateIndex: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/online-learning',{
+//     useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     useUnifiedTopology: true
+// });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
 
 
 
